a4b202f83042bb4e523a685abb1be904b149a799
Ificiana <ificiana@gmail.com>
2024-05-23 17:37:06 +0530

    apply prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # study-re-zero
+
 A quickly put together project to help me list and study the words/kanji from Re:Zero LN
--- script.js
+++ script.js
@@ -1,88 +1,93 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let studiedData = JSON.parse(localStorage.getItem('studiedData')) || {};
-    const urlParams = new URLSearchParams(window.location.search);
-    const kanjiFilter = urlParams.get('k');
-    
-    createContent(wordsData, studiedData, kanjiFilter);
+document.addEventListener("DOMContentLoaded", function () {
+  let studiedData = JSON.parse(localStorage.getItem("studiedData")) || {};
+  const urlParams = new URLSearchParams(window.location.search);
+  const kanjiFilter = urlParams.get("k");
 
-    document.getElementById('loadData').addEventListener('click', function() {
-    document.getElementById('fileInput').click();
-});
+  createContent(wordsData, studiedData, kanjiFilter);
 
-document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  document.getElementById("loadData").addEventListener("click", function () {
+    document.getElementById("fileInput").click();
+  });
 
-    reader.onload = function() {
+  document
+    .getElementById("fileInput")
+    .addEventListener("change", function (event) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = function () {
         const jsonData = JSON.parse(reader.result);
-        localStorage.setItem('studiedData', JSON.stringify(jsonData));
-        alert('Data loaded to localStorage');
-    };
+        localStorage.setItem("studiedData", JSON.stringify(jsonData));
+        alert("Data loaded to localStorage");
+      };
 
-    reader.readAsText(file);
-});
-
-
-    document.getElementById('exportData').addEventListener('click', function() {
-        let studiedData = JSON.parse(localStorage.getItem('studiedData')) || {};
-        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(studiedData));
-        const downloadAnchorNode = document.createElement('a');
-        downloadAnchorNode.setAttribute("href", dataStr);
-        downloadAnchorNode.setAttribute("download", "studied.json");
-        document.body.appendChild(downloadAnchorNode); // required for firefox
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
+      reader.readAsText(file);
     });
 
-    function createContent(wordsData, studiedData, kanjiFilter) {
-        const toc = document.getElementById('toc');
-        const content = document.getElementById('content');
+  document.getElementById("exportData").addEventListener("click", function () {
+    let studiedData = JSON.parse(localStorage.getItem("studiedData")) || {};
+    const dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(studiedData));
+    const downloadAnchorNode = document.createElement("a");
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "studied.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  });
 
-        Object.keys(wordsData).forEach(kanji => {
-            const tocEntry = document.createElement('div');
-            tocEntry.innerHTML = `<a href="?k=${kanji}#${kanji}">${kanji}</a>`;
-            toc.appendChild(tocEntry);
-        });
+  function createContent(wordsData, studiedData, kanjiFilter) {
+    const toc = document.getElementById("toc");
+    const content = document.getElementById("content");
 
-        const kanji = kanjiFilter;
+    Object.keys(wordsData).forEach((kanji) => {
+      const tocEntry = document.createElement("div");
+      tocEntry.innerHTML = `<a href="?k=${kanji}#${kanji}">${kanji}</a>`;
+      toc.appendChild(tocEntry);
+    });
 
-        const kanjiSection = document.createElement('div');
-        kanjiSection.classList.add('kanji-section');
-        kanjiSection.id = kanji;
+    const kanji = kanjiFilter;
 
-        const h1 = document.createElement('h1');
-        h1.textContent = kanji;
-        kanjiSection.appendChild(h1);
+    const kanjiSection = document.createElement("div");
+    kanjiSection.classList.add("kanji-section");
+    kanjiSection.id = kanji;
 
-        const ul = document.createElement('ul');
+    const h1 = document.createElement("h1");
+    h1.textContent = kanji;
+    kanjiSection.appendChild(h1);
 
-        if (kanji) {
-            wordsData[kanji].forEach(word => {
-                if (!studiedData[kanji] || !studiedData[kanji].includes(word)) {
-                    const li = document.createElement('li');
-                    const checkbox = document.createElement('input');
-                    checkbox.type = 'checkbox';
-                    checkbox.addEventListener('click', () => handleCheckboxClick(kanji, word));
-                    li.appendChild(checkbox);
-                    li.appendChild(document.createTextNode(` ${word}`));
-                    ul.appendChild(li);
-                }
-            });
+    const ul = document.createElement("ul");
 
-            kanjiSection.appendChild(ul);
-            content.appendChild(kanjiSection);
+    if (kanji) {
+      wordsData[kanji].forEach((word) => {
+        if (!studiedData[kanji] || !studiedData[kanji].includes(word)) {
+          const li = document.createElement("li");
+          const checkbox = document.createElement("input");
+          checkbox.type = "checkbox";
+          checkbox.addEventListener("click", () =>
+            handleCheckboxClick(kanji, word),
+          );
+          li.appendChild(checkbox);
+          li.appendChild(document.createTextNode(` ${word}`));
+          ul.appendChild(li);
         }
+      });
+
+      kanjiSection.appendChild(ul);
+      content.appendChild(kanjiSection);
     }
+  }
 
-    function handleCheckboxClick(kanji, word) {
-        let studiedData = JSON.parse(localStorage.getItem('studiedData')) || {};
+  function handleCheckboxClick(kanji, word) {
+    let studiedData = JSON.parse(localStorage.getItem("studiedData")) || {};
 
-        if (!studiedData[kanji]) {
-            studiedData[kanji] = [];
-        }
-        studiedData[kanji].push(word);
-
-        localStorage.setItem('studiedData', JSON.stringify(studiedData));
-        location.reload();
+    if (!studiedData[kanji]) {
+      studiedData[kanji] = [];
     }
+    studiedData[kanji].push(word);
+
+    localStorage.setItem("studiedData", JSON.stringify(studiedData));
+    location.reload();
+  }
 });
